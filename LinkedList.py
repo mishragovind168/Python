f9242b929d28fb1af94bdc4c2343ee4472bdfc84
mishragovind168 <85922132+mishragovind168@users.noreply.github.com>
2022-07-30 12:28:28 +0530

    Create LinkedList.py

---
+++ LinkedList.py
@@ -0,0 +1,58 @@
+#2
+#Linked List Implentation
+
+class Node:
+    def __init__(self,data=None,next=None):
+        self.data = data
+        self.next = next
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+# Insert element at end            
+    def insert_end(self,data):
+        if self.head is None:
+            self.head = Node(data,None)
+        else:
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = Node(data,None)
+            
+# Insert element at start    
+    def insert_start(self,data):
+        if self.head is None:
+            self.head = Node(data,None)
+        else:
+            node = Node(data,self.head)
+            self.head = node
+            
+    def delete_first(self):
+        if self.head is None:
+            print("LinkList is emPity")
+        else:
+            self.head.data = None
+            
+    
+#Print function            
+    def print(self):
+        if self.head is None:
+            print("LinkedList is empity")
+            
+        else:
+            temp = self.head
+            lstr = ''
+            while temp is not None:
+                lstr+= str(temp.data)+'--> '
+                temp = temp.next
+
+            print(lstr)
+            
+obj = LinkedList()
+obj.insert_start(1)
+obj.insert_start(4)
+obj.insert_end(45)
+obj.print()
+obj.delete_first()
+obj.print()
